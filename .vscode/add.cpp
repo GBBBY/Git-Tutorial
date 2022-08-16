d229b3ec6a5852585815c7256279c1503f058f50
GBBBY <me87654@naver.com>
2022-08-17 01:11:29 +0900

    add

---
+++ .vscode/add.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main(void)
+{
+ int val1, val2;
+ std::cout<<"F,S number : ";
+ std::cin>>val1>>val2;
+ 
+ int result=val1+val2;
+
+std::cout<<"Addition : "<<result<<std::endl;
+ return 0;
+
+}
